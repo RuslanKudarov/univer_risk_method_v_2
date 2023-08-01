7ce8221bbe11c490fba56a40cb26864e61a800f4
RuslanKudarov <139555609+RuslanKudarov@users.noreply.github.com>
2023-08-01 06:19:17 +0300

    method2.py

--- method2.py
+++ method2.py
@@ -139,10 +139,11 @@ def run():
             if check_stud:
                 st.write(df_stud)
 
-        if len(df_mon_all.index) == len(df_mon_neat.index) + len(df_mon_neud.index) + len(df_mon_usp.index):
-            go_2 = st.button('Обработать данные')
-            if go_2:
-                st.write(":smile:")                
+        if len(df_ab_usp.index) > 0 and len(df_mon_all.index) > 0 and len(df_stud.index) > 0:
+            if len(df_mon_all.index) == len(df_mon_neat.index) + len(df_mon_neud.index) + len(df_mon_usp.index):
+                go_2 = st.button('Обработать данные')
+                if go_2:
+                    st.write(":smile:")                
 
 # подготовка данных
     
